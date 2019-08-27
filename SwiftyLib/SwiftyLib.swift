b687042e5319012e6ddcb22adf17929c11d7fb04
Pioneer Wang <pioneer@Pioneers-Mac.local>
2019-08-26 23:37:29 -0700

    Added test cases

---
+++ SwiftyLib/SwiftyLib.swift
@@ -0,0 +1,19 @@
+//
+//  SwiftyLib.swift
+//  SwiftyLib
+//
+//  Created by Pioneer Wang on 2019/8/26.
+//  Copyright © 2019 Pioneer Wang. All rights reserved.
+//
+
+public final class SwiftyLib {
+    let name = "SwiftyLib"
+
+    public func add(a: Int, b:Int) -> Int {
+        return a + b
+    }
+    public func sub(a: Int, b:Int) -> Int {
+        return a - b
+    }
+}
+
--- SwiftyLibTests/SwiftyLibTests.swift
+++ SwiftyLibTests/SwiftyLibTests.swift
@@ -11,24 +11,16 @@ import XCTest
 
 class SwiftyLibTests: XCTestCase {
 
+    var swiftyLib: SwiftyLib!
+    
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        swiftyLib = SwiftyLib()
     }
 
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func testAdd() {
+        XCTAssertEqual(swiftyLib.add(a: 1, b: 1), 2)
     }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testSub() {
+        XCTAssertEqual(swiftyLib.sub(a: 2, b: 1), 1)
     }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
